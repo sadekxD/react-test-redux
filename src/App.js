73215085b116bda79329db0d6219a391569951e1
unknown <sadekirfan3@gmail.com>
2021-12-31 09:35:13 +0600

    code splitted

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import "./App.css";
+import React, { lazy, Suspense } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Form, FormControl } from "react-bootstrap";
-import { useState } from "react";
-import Select from "react-select";
 import { Link, Route, Switch } from "react-router-dom";
-import JsonForm from "./components/JsonForm";
-import Counter from "./components/Counter";
+
+const JsonForm = lazy(() => import("./components/JsonForm"));
+const Counter = lazy(() => import("./components/Counter"));
 
 function App() {
-  return (
-    <div className="App my-5">
-      <Link to="/">Home</Link>
-      <br />
-      <Link to="/counter">Counter</Link>
-      <Switch>
-        <Route exact path="/">
-          <JsonForm />
-        </Route>
-        <Route exact path="/counter">
-          <Counter />
-        </Route>
-      </Switch>
-    </div>
-  );
+	return (
+		<div className="App my-5">
+			<Link to="/">Home</Link>
+			<br />
+			<Link to="/counter">Counter</Link>
+			<Switch>
+				<Suspense fallback={<div>Loading Page</div>}>
+					<Route exact path="/">
+						<JsonForm />
+					</Route>
+					<Route exact path="/counter">
+						<Counter />
+					</Route>
+				</Suspense>
+				<Route>
+					<h1>Page not found</h1>
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
